716bae340722f966b18bcdec995638d455059bb7
MPalamut <palamuto@live.de>
2026-05-29 09:32:49 +0200

    new

--- frontend/src/Input.jsx
+++ frontend/src/Input.jsx
@@ -71,7 +71,7 @@ export default function Input() {
         }
         responseReceived()
     }
-    
+
     return (
         <>
             <div className={`${styles.inputContainer} ${loading ? styles.loading : ""}`}>
