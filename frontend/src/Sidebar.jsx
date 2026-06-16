f4a5aa30e4e6431962914752acc03cdbe0824221
MPalamut <palamuto@live.de>
2026-06-16 14:21:51 +0200

    new

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -7,10 +7,8 @@ import { RxHamburgerMenu, RxPencil2, RxGear, RxSun, RxMoon } from "react-icons/r
 import { getStore } from "./Store.jsx"
 
 export default function Sidebar() {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
     const [settingsOpen, setSettingsOpen] = useState(false);
     const settingsOpenRef = useRef();
-
     const { theme, setTheme, temperature, setTemperature } = getStore();
 
     useEffect(() => {
@@ -31,7 +29,7 @@ export default function Sidebar() {
 
     return (
         <>
-            <div className={styles.sidebar} style={{ width: sidebarOpen ? "15vw" : "9vw" }}>
+            <div className={styles.sidebar}>
                 <div className={styles.header}>
                     <img className={styles.icon} src={icon} alt="Your Ai" />
                     <button className={styles.sidebarBtn} title="Neuer Chat" onClick={() => {
@@ -41,8 +39,6 @@ export default function Sidebar() {
                     }
                     ><RxPencil2 />
                     </button>
-
-                    {/* <button className={styles.sidebarBtn} title="Menü" onClick={() => setSidebarOpen(!sidebarOpen)}><RxHamburgerMenu /></button> */}
                     <button className={styles.sidebarBtn} title="Farbbmodus wechseln" onClick={() => setTheme(theme === "root" ? "light" : "root")}>{theme === "root" ? <RxSun /> : <RxMoon />}</button>
                     <select className={styles.temperature} name="temperature" id="temperature" value={temperature} onChange={e => setTemperature(parseFloat(e.target.value))}>
                         <option value="0.1">Schnelles Denken</option>
