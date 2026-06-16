16ec0235610f1ec4e4457f1da86497263ec18f1d
MPalamut <palamuto@live.de>
2026-06-16 14:25:16 +0200

    new

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -48,7 +48,7 @@ export default function Sidebar() {
                     {<Accessiblity />}
                 </div>
                 <div className="footer">
-                    <button className={styles.sidebarBtn} title="Einstellungen" onClick={() => { setSettingsOpen(!settingsOpen)}}><RxGear /></button>
+                    <button className={styles.sidebarBtn} title="Einstellungen" onClick={() => { setSettingsOpen(!settingsOpen) }}><RxGear /></button>
                     {settingsOpen && <div className={styles.settings} ref={settingsOpenRef}>
                         <ul>
                             <li><a href="#">Sprachsteuerung</a></li>
