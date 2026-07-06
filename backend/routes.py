b760bdd369dff5d2a4bb14716ef071fd5dbccf52
MPalamut <palamuto@live.de>
2026-07-06 15:29:33 +0200

    new

--- backend/routes.py
+++ backend/routes.py
@@ -57,21 +57,22 @@ def register(data: dict):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-    existing_user = cursor.fetchone()
-    
-    if existing_user:
-        conn.close()
-        return {"status": "error", "message": "Benutzername existiert bereits!"}
-    
-    password_bytes = password.encode('utf-8')
-    hashed_password = hashlib.sha256(password_bytes).hexdigest()
+    try: 
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        existing_user = cursor.fetchone()
 
-    try:
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
-        conn.commit() 
-        status = "success"
-        message = f"Registrierung erfolgreich! Willkommen, {username}."
+        if existing_user:
+            status = "error"
+            message = "Benutzername existiert bereits"
+        
+        else:
+            password_bytes = password.encode('utf-8')
+            hashed_password = hashlib.sha256(password_bytes).hexdigest()
+
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+            conn.commit()
+            status = "success"
+            message = f"Registrierung erfolgreich! Willkommen, {username}."
     except sqlite3.Error as e:
         status = "error"
         message = f"Datenbankfehler: {str(e)}"
@@ -85,20 +86,29 @@ def login(data: dict, response: Response):
     username = data.get("username")
     password = data.get("password")
 
+    password_bytes = password.encode('utf-8')
+    hashed_password = hashlib.sha256(password_bytes).hexdigest()
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
-    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
-    user = cursor.fetchone()
-
-    if user:
-        conn.commit()
-        conn.close()
-        return {"status": "success", "message": f"Willkommen zurück, {username}!"}    
-    else:
-        conn.close()
-        return {"status": "error", "message": "Falscher Benutzername oder Passwort!"}
+    try:
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password))
+        user = cursor.fetchone()
+        if user:
+            status = "success"
+            message = f"Willkommen zurück, {username}!"
+        else:  
+            status = "error"
+            message = "Falscher Benutzername oder Passwort!"
+    except sqlite3.Error as e:
+        status = "error"
+        message = f"Datenbankfehler: {str(e)}"
+    finally:
+        conn.close
     
+    return {"status": status, "message": message}
+   
 @router.post("/report")
 async def report(payload: dict):
     username = payload.get("username")
